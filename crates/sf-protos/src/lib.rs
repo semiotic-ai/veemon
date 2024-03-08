3363ceb86ee11b050ad59b3f2fe2b8e6342e26bc
pedro bufulin <pedro@semiotic.com>
2024-03-07 23:37:41 -0300

    refactor: remove outter mod

--- crates/sf-protos/src/lib.rs
+++ crates/sf-protos/src/lib.rs
@@ -2,19 +2,18 @@
 //!
 //! This module provides access to Rust implementations of StreamingFast's protocol buffer definitions,
 //! enabling the encoding and decoding of data for Ethereum blockchain and bstream services.
-pub mod sf {
-    //! Module for Ethereum-related data structures and operations.
-    //! Currently contains the `.proto` defined [here](https://github.com/streamingfast/firehose-ethereum/blob/d9ec696423c2288db640f00026ae29a6cc4c2121/proto/sf/ethereum/type/v2/type.proto#L9)    
-    pub mod ethereum {
-        pub mod r#type {
-            pub mod v2 {
-                include!(concat!(env!("OUT_DIR"), "/sf.ethereum.r#type.v2.rs"));
-            }
+
+/// Module for Ethereum-related data structures and operations.
+/// Currently contains the `.proto` defined [here](https://github.com/streamingfast/firehose-ethereum/blob/d9ec696423c2288db640f00026ae29a6cc4c2121/proto/sf/ethereum/type/v2/type.proto#L9)    
+pub mod ethereum {
+    pub mod r#type {
+        pub mod v2 {
+            include!(concat!(env!("OUT_DIR"), "/sf.ethereum.r#type.v2.rs"));
         }
     }
-    pub mod bstream {
-        pub mod v1 {
-            include!(concat!(env!("OUT_DIR"), "/sf.bstream.v1.rs"));
-        }
+}
+pub mod bstream {
+    pub mod v1 {
+        include!(concat!(env!("OUT_DIR"), "/sf.bstream.v1.rs"));
     }
 }
