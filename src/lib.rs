fd50f52b8e1a2453777ecafb0067e80310dbbb28
Joseph Livesey <joseph@semiotic.ai>
2024-07-26 11:50:48 -0400

    fix: use forked lighthouse extended for historical roots

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ mod tests {
     use super::*;
 
     use beacon_node::beacon_chain::types::MainnetEthSpec;
-    use types::light_client_update::FINALIZED_ROOT_INDEX;
+    use types::light_client_update::HISTORICAL_ROOTS_INDEX;
 
     const HEAD_STATE_JSON: &str = include_str!("../head-state.json");
 
@@ -24,17 +24,16 @@ mod tests {
 
         let proof = state
             .data
-            .compute_merkle_proof(FINALIZED_ROOT_INDEX)
+            .compute_merkle_proof(HISTORICAL_ROOTS_INDEX)
             .unwrap();
 
         insta::assert_debug_snapshot!(proof, @r###"
         [
-            0x2084040000000000000000000000000000000000000000000000000000000000,
-            0x29fd8c86a3d314172f4d273d90691fdb8c73acb464a2acdf865d370a5a91f6ea,
-            0x985137d97513dd35feaf1617f7e8d985fa251b68244b63373cf9c50376fd2bfd,
-            0xf0e45527b9da9c9266caeabcaf03ffb9cbc3576888b3f8628f9372d9d03f1feb,
-            0x50d36fdf9425494f04a6e5cd996855b7c1b7fb581bab17e3e97c5e48d232d1f2,
-            0x2150a88f205759c59817f42dc307620c67d3d23417959286928d186c639a0948,
+            0xe81a79506c46b126f75a08cdd5cbc35052b61ca944c6c3becf32432e2ee6373a,
+            0xcfb49cd7eb0051153685e5e6124b635c6b9bcc69a6ead6af0ef7d9885fcc16e2,
+            0x29c2e1f6d96493e9b49517cb78123990038429e4c3574688a48f9abe69238449,
+            0xdb329a01d9114f087155633b36b498c8e60028c0acedc8e3b64e013dbbd4fa06,
+            0x53b107024e402f616f8f348d900e0d62f4b6f0558d2bfbd09200e68620a5b9c2,
         ]
         "###);
     }
