7e33717169fd25e9be52f2fd37a059039e8251a1
Joseph Livesey <joseph@semiotic.ai>
2024-08-13 13:14:23 -0400

    refactor: handle prost config in separate method

--- crates/sf-protos/build.rs
+++ crates/sf-protos/build.rs
@@ -1,16 +1,22 @@
 use prost_wkt_build::*;
 use std::{env, io::Result, path::PathBuf};
 
+fn configure_prost() -> prost_build::Config {
+    let mut config = prost_build::Config::new();
+    config
+        .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
+        .extern_path(".google.protobuf.Any", "::prost_wkt_types::Any")
+        .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
+        .extern_path(".google.protobuf.Value", "::prost_wkt_types::Value");
+    config
+}
+
 fn main() -> Result<()> {
     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
     let descriptor_file = out.join("descriptors.bin");
 
-    let mut prost_build = prost_build::Config::new();
+    let mut prost_build = configure_prost();
     prost_build
-        .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
-        .extern_path(".google.protobuf.Any", "::prost_wkt_types::Any")
-        .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
-        .extern_path(".google.protobuf.Value", "::prost_wkt_types::Value")
         .file_descriptor_set_path(&descriptor_file)
         .compile_protos(
             &["src/protos/block.proto", "src/protos/bstream.proto"],
