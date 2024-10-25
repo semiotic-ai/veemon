e292f4c85a4865436d466d9c88a7304f57f52047
Joseph Livesey <joseph@semiotic.ai>
2024-10-25 09:26:01 -0400

    fix(flat-files-decoder): include error message content in error messages

--- crates/flat-files-decoder/src/headers/error.rs
+++ crates/flat-files-decoder/src/headers/error.rs
@@ -3,11 +3,11 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum BlockHeaderError {
-    #[error("Read error")]
+    #[error("Read error: {0}")]
     ReadError(#[from] std::io::Error),
-    #[error("JSON Error")]
+    #[error("JSON Error: {0}")]
     JsonError(#[from] serde_json::Error),
-    #[error("Mismatched roots")]
+    #[error("Mismatched roots: {0:?}")]
     MismatchedRoots(Box<(BlockHeaderRoots, BlockHeaderRoots)>),
     #[error("Missing header")]
     MissingHeader,
