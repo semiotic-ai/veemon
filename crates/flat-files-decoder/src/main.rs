c247805a6442881b0841b20a937f2dce5f267a2c
Joseph Livesey <joseph@semiotic.ai>
2024-10-30 17:57:20 -0400

    refactor(decoder): reorganize entrypoint to bin

--- crates/flat-files-decoder/src/main.rs
+++ crates/flat-files-decoder/src/main.rs
@@ -1,6 +1,74 @@
 use clap::{Parser, Subcommand};
-use flat_files_decoder::{decode_flat_files, stream_blocks, Decompression};
-use std::io::{self, BufReader, BufWriter};
+use flat_files_decoder::{decode_flat_files, error::DecodeError, stream_blocks, Decompression};
+use std::{
+    io::{self, BufReader, BufWriter},
+    process::ExitCode,
+};
+use tracing::{error, info, level_filters::LevelFilter, subscriber::set_global_default};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+#[tokio::main]
+async fn main() -> ExitCode {
+    init_tracing();
+    if let Err(e) = run().await {
+        error!("Decoder error: {e}");
+        return ExitCode::from(1);
+    }
+    ExitCode::SUCCESS
+}
+
+fn init_tracing() {
+    let filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env_lossy();
+    let subscriber_builder: tracing_subscriber::fmt::SubscriberBuilder<
+        tracing_subscriber::fmt::format::DefaultFields,
+        tracing_subscriber::fmt::format::Format,
+        EnvFilter,
+    > = FmtSubscriber::builder().with_env_filter(filter);
+    set_global_default(subscriber_builder.with_ansi(true).pretty().finish()).expect(
+        "Failed to set up the global default subscriber for logging. Please check if the RUST_LOG environment variable is set correctly.",
+    );
+}
+
+async fn run() -> Result<(), DecodeError> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Stream {
+            decompression,
+            end_block,
+        } => match decompression {
+            Decompression::Zstd => {
+                let reader = zstd::stream::Decoder::new(io::stdin())?;
+                let writer = BufWriter::new(io::stdout().lock());
+                stream_blocks(reader, writer, end_block).await
+            }
+            Decompression::None => {
+                let reader = BufReader::with_capacity((64 * 2) << 20, io::stdin().lock());
+                let writer = BufWriter::new(io::stdout().lock());
+                stream_blocks(reader, writer, end_block).await
+            }
+        },
+        Commands::Decode {
+            input,
+            headers_dir,
+            output,
+            decompression,
+        } => {
+            let blocks = decode_flat_files(
+                input,
+                output.as_deref(),
+                headers_dir.as_deref(),
+                decompression,
+            )?;
+
+            info!("Total blocks: {}", blocks.len());
+
+            Ok(())
+        }
+    }
+}
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -36,47 +104,4 @@ enum Commands {
         /// optionally decompress zstd compressed flat files
         decompression: Decompression,
     },
-}
-#[tokio::main]
-async fn main() {
-    let cli = Cli::parse();
-
-    match cli.command {
-        Commands::Stream {
-            decompression,
-            end_block,
-        } => match decompression {
-            Decompression::Zstd => {
-                let reader =
-                    zstd::stream::Decoder::new(io::stdin()).expect("Failed to create zstd decoder");
-                let writer = BufWriter::new(io::stdout().lock());
-                stream_blocks(reader, writer, end_block)
-                    .await
-                    .expect("Failed to stream blocks");
-            }
-            Decompression::None => {
-                let reader = BufReader::with_capacity((64 * 2) << 20, io::stdin().lock());
-                let writer = BufWriter::new(io::stdout().lock());
-                stream_blocks(reader, writer, end_block)
-                    .await
-                    .expect("Failed to stream blocks");
-            }
-        },
-        Commands::Decode {
-            input,
-            headers_dir,
-            output,
-            decompression,
-        } => {
-            let blocks = decode_flat_files(
-                input,
-                output.as_deref(),
-                headers_dir.as_deref(),
-                decompression,
-            )
-            .expect("Failed to decode files");
-
-            println!("Total blocks: {}", blocks.len());
-        }
-    }
 }
