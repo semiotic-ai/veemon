144f29da944528a3c23cf844df9d880d234112b7
Joseph Livesey <joseph@semiotic.ai>
2024-11-06 12:39:55 -0500

    refactor(header-accumulator): clean up tests dependent on binary files

--- crates/header-accumulator/examples/era_validator.rs
+++ crates/header-accumulator/examples/era_validator.rs
@@ -9,11 +9,14 @@ fn create_test_reader(path: &str) -> BufReader<File> {
     BufReader::new(File::open(path).unwrap())
 }
 
-#[test]
-fn test_era_validate() -> Result<(), EraValidateError> {
+fn main() -> Result<(), EraValidateError> {
     let mut headers: Vec<ExtHeaderRecord> = Vec::new();
+
     for number in (0..=8200).step_by(100) {
-        let file_name = format!("tests/ethereum_firehose_first_8200/{:010}.dbin", number);
+        let file_name = format!(
+            "your-test-assets/ethereum_firehose_first_8200/{:010}.dbin",
+            number
+        );
         let reader = create_test_reader(&file_name);
         let blocks = read_blocks_from_reader(reader, Compression::None).unwrap();
         let successful_headers = blocks
@@ -23,6 +26,7 @@ fn test_era_validate() -> Result<(), EraValidateError> {
             .collect::<Result<Vec<_>, _>>()?;
         headers.extend(successful_headers);
     }
+
     assert_eq!(headers.len(), 8300);
     assert_eq!(headers[0].block_number, 0);
 
@@ -30,12 +34,13 @@ fn test_era_validate() -> Result<(), EraValidateError> {
     let epoch: Epoch = headers.try_into().unwrap();
 
     let result = premerge_accumulator.validate_era(&epoch)?;
-
     let expected = Hash256::new([
         94, 193, 255, 184, 195, 177, 70, 244, 38, 6, 199, 76, 237, 151, 61, 193, 110, 197, 161, 7,
         192, 52, 88, 88, 195, 67, 252, 148, 120, 11, 66, 24,
     ]);
     assert_eq!(result, expected);
+
+    println!("Era validated successfully!");
 
     Ok(())
 }
