6d8f1de2bf3e6106492f44a65d0779e954959715
Joseph Livesey <joseph@semiotic.ai>
2024-11-06 12:39:55 -0500

    refactor(header-accumulator): clean up tests dependent on binary files

--- crates/header-accumulator/examples/inclusion_proof.rs
+++ crates/header-accumulator/examples/inclusion_proof.rs
@@ -10,14 +10,13 @@ fn create_test_reader(path: &str) -> BufReader<File> {
     BufReader::new(File::open(path).unwrap())
 }
 
-#[test]
-fn test_inclusion_proof() -> Result<(), EraValidateError> {
+fn main() -> Result<(), EraValidateError> {
     let mut headers: Vec<ExtHeaderRecord> = Vec::new();
-    let mut all_blocks: Vec<Block> = Vec::new(); // Vector to hold all blocks
+    let mut all_blocks: Vec<Block> = Vec::new();
 
     for flat_file_number in (0..=8200).step_by(100) {
         let file = format!(
-            "tests/ethereum_firehose_first_8200/{:010}.dbin",
+            "your-test-assets/ethereum_firehose_first_8200/{:010}.dbin",
             flat_file_number
         );
         match read_blocks_from_reader(create_test_reader(&file), Compression::None) {
@@ -28,7 +27,7 @@ fn test_inclusion_proof() -> Result<(), EraValidateError> {
                         .map(|block| ExtHeaderRecord::try_from(block).unwrap())
                         .collect::<Vec<ExtHeaderRecord>>(),
                 );
-                all_blocks.extend(blocks); // Extend the all_blocks vector with the decoded blocks
+                all_blocks.extend(blocks);
             }
             Err(e) => {
                 eprintln!("error: {:?}", e);
@@ -42,8 +41,7 @@ fn test_inclusion_proof() -> Result<(), EraValidateError> {
     let inclusion_proof =
         generate_inclusion_proof(headers, start_block, end_block).unwrap_or_else(|e| {
             println!("Error occurred: {}", e);
-            // Handle the error, e.g., by exiting the program or returning a default value
-            std::process::exit(1); // Exiting the program, for example
+            std::process::exit(1);
         });
     assert_eq!(
         inclusion_proof.len() as usize,
@@ -54,9 +52,11 @@ fn test_inclusion_proof() -> Result<(), EraValidateError> {
     let proof_blocks: Vec<Block> = all_blocks[start_block as usize..=end_block as usize].to_vec();
     assert!(verify_inclusion_proof(proof_blocks, None, inclusion_proof.clone()).is_ok());
 
-    // verify if inclusion proof fails on not proven blocks
+    // Verify if inclusion proof fails on not proven blocks
     let proof_blocks: Vec<Block> = all_blocks[302..=403].to_vec();
     assert!(verify_inclusion_proof(proof_blocks, None, inclusion_proof.clone()).is_err());
+
+    println!("Inclusion proof verified successfully!");
 
     Ok(())
 }
