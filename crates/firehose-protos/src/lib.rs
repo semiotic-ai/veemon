fc2d453f2a73ac6df145df7b49add7a7760e7186
Joseph Livesey <joseph@semiotic.ai>
2024-11-08 18:17:55 -0500

    docs(protos): refine rust docs for public api

--- crates/firehose-protos/src/lib.rs
+++ crates/firehose-protos/src/lib.rs
@@ -27,8 +27,20 @@ pub use error::ProtosError;
 pub use ethereum_v2::{eth_block::FullReceipt, Block as EthBlock, BlockHeader};
 pub(crate) use firehose_v2::single_block_request::BlockNumber;
 
-/// Work with the Firehose v2 API.
-pub use firehose_v2::{
-    fetch_client::FetchClient, stream_client::StreamClient, Request, Response, SingleBlockRequest,
-    SingleBlockResponse,
-};
+/// Interact programatically with the Firehose v2 Fetch API.
+pub use firehose_v2::fetch_client::FetchClient;
+
+/// Create Firehose API fetch requests.
+pub use firehose_v2::Request;
+
+/// Work with Firehose API streaming responses.
+pub use firehose_v2::Response;
+
+/// Create Firehose API streaming requests.
+pub use firehose_v2::SingleBlockRequest;
+
+/// Receive Firehose API fetch responses.
+pub use firehose_v2::SingleBlockResponse;
+
+/// Work with the Firehose v2 Stream API.
+pub use firehose_v2::stream_client::StreamClient;
