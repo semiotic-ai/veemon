880c05fe07d4b6571ccf2e859868a383dfc629f8
Joseph Livesey <joseph@semiotic.ai>
2024-11-26 10:18:04 -0500

    refactor: be consistent naming errors in client and protos

--- crates/firehose-client/src/error.rs
+++ crates/firehose-client/src/error.rs
@@ -3,15 +3,15 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum ClientError {
+    #[error("Missing environment variable: {0}")]
+    EnvVarMissing(#[from] dotenvy::Error),
+
     #[error("gRPC error: {0}")]
     GRpc(#[from] tonic::transport::Error),
 
-    #[error("Invalid URI: {0}")]
-    InvalidUri(#[from] InvalidUri),
-
-    #[error("Missing environment variable: {0}")]
-    MissingEnvVar(#[from] dotenvy::Error),
-
     #[error("{0}")]
     TonicStatus(#[from] tonic::Status),
+
+    #[error("Invalid URI: {0}")]
+    UriInvalid(#[from] InvalidUri),
 }
