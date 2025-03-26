213e08216898adf2dff02f1fc6db7680759bcfd9
Severiano K Sisneros IV <severiano@semiotic.ai>
2025-03-26 17:28:04 -0500

    docs(arbitrum-ve): Added license headers

--- crates/arbitrum-ve/src/errors.rs
+++ crates/arbitrum-ve/src/errors.rs
@@ -1,3 +1,6 @@
+// Copyright 2024-, Semiotic AI, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 /// Arbitrum verification errors
 #[derive(thiserror::Error, Debug)]
 pub enum ArbitrumValidateError {
--- crates/arbitrum-ve/src/lib.rs
+++ crates/arbitrum-ve/src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright 2024-, Semiotic AI, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 mod errors;
 mod inclusion_proof;
 
